6b963a2aa9829e2fe8b61101990f7ed4b62926cf
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-09-04 18:37:54 +0300

    creating function to save new credentials

--- credentials.py
+++ credentials.py
@@ -3,6 +3,8 @@ class Credentials:
     Credentials class to create new credential instances
     """
 
+    credentials_list = []
+
     def __init__(self, site_name, user_name, password):
         """
         __init__ to define properties of new Credentials
@@ -15,3 +17,9 @@ class Credentials:
         self.site_name = site_name
         self.user_name = user_name
         self.password = password
+
+    def save_credentials(self):
+        """
+        save_credentials saves new instances to credentials_list array
+        """
+        Credentials.credentials_list.append(self)
