6458a229700608e52d1aa0e0641850566ddbd92c
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-09-04 18:21:31 +0300

    create class for testing user class

---
+++ user_login_test.py
@@ -0,0 +1,11 @@
+import unittest
+from user_login import User
+
+
+class TestUser(unittest.TestCase):
+    """
+    Test class to define test cases for the User class
+
+    Args:
+        unittest.TestCase: TestCase class creates test cases
+    """
